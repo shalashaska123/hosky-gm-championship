a3eb4a7196c2165a8a694bb2debde263bda7830a
shalashaska123 <shalashaska123@googlemail.com>
2026-07-20 06:35:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,10 @@
 const CHAMPIONSHIP_CONFIG = {
   tournamentName: "HOSKY GM CHAMPIONSHIP",
   isCupActive: false,
-
   leagueIds: {
-    classic: "262665",
+    classic: "262665", // <--- CHANGE THIS TO YOUR REAL LEAGUE ID (e.g., "123456")
     h2h: ""           
   },
-
-  prizes: {
-    totalPool: "",
-    classicLeague: "",
-    headToHead: "",
-    hoskyCup: "",
-    teamOfTheMonth: ""
-  },
-
-  nft: {
-    policyId: "",      
-    blockfrostKey: ""
-  }
+  prizes: { totalPool: "", classicLeague: "", headToHead: "", hoskyCup: "", teamOfTheMonth: "" },
+  nft: { policyId: "", blockfrostKey: "" }
 };
